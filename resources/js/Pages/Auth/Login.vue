25c801aa61e4fe7b6bdae7819e4c8348af3371cd
DaniloVoid <danilovsdanilo@gmail.com>
2022-11-04 17:51:31 -0400

    add login layout

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,67 +1,51 @@
-<script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+<template>
+    <div style="height: 100vh;" class="d-flex justify-center align-center">
+        <div class="my-4" style="width: 50%;">
 
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
+            <v-img height="100" src="https://boavista.rr.gov.br/publico/img/logos/logo_2022_v1.png" class="my-10">
+            </v-img>
 
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false
-});
+            <v-card>
+                <v-card-title primary-title>
+                    Fazer Login
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-card-text>
+                    <v-form>
+                        <v-text-field label="E-mail" v-model="user.email"></v-text-field>
+                        <v-text-field type="password" label="Senha" v-model="user.password"></v-text-field>
+                    </v-form>
 
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-btn block color="primary" dark>Entrar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+import LoginLayoutBaseVue from '@/layouts/LoginLayoutBase.vue';
+import { ref } from '@vue/reactivity';
+
+export default {
+    layout: LoginLayoutBaseVue,
+
+    setup() {
+
+        const user = ref({})
+
+        function login() {
+
+        }
+
+        return { user }
+    }
+}
 </script>
 
-<template>
-    <GuestLayout>
-        <Head title="Log in" />
+<style>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
-</template>
+</style>
